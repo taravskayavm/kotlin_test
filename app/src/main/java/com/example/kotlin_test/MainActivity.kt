8f507e1638d466820cc2138a4578c73f1f32f0ab
taravskayavm <taravskayavm@gmail.com>
2023-03-03 17:47:13 +0300

    Lesson 1, Start

--- app/src/main/java/com/example/kotlin_test/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_test/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.kotlin_test
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_test.lesson1.City
+import com.example.kotlin_test.lesson1.test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test("")
+
+        City(name="newName", temperature=20).test()
     }
+
 }
+
+
---
+++ app/src/main/java/com/example/kotlin_test/lesson1/City.kt
@@ -0,0 +1,47 @@
+package com.example.kotlin_test.lesson1
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+open class City constructor (val name:String="defaultName", var temperature: Int=15) {
+
+    fun test(testParam: String="Stroka") {
+        testParam2 = "Stroka2"
+        val testParam3 = testParam2
+    }
+
+    var  p = ""
+    get() {
+        return field
+    }
+    set(p0:String){
+        field = p0
+    }
+
+
+    lateinit var testParam2: String //TODO Int примитив (127)
+
+
+    constructor (name:String,temperature:Int,descr:String):this(name,temperature){
+    }
+    constructor (name:String,temperature:Int,descr1:String,descr2:String):this(name,temperature,descr1){
+    }
+    constructor(name:String,temperature:Int,descr1:String,descr2:String,descr3:String):this(name,temperature,descr1,descr2){
+    }
+
+}
+
+fun test(testParam:String) {
+}
+
+
+class CityName : City(), View.OnClickListener{
+    override fun onClick(p0: View?) {
+    }
+
+}
+
+class Button @JvmOverloads constructor(context: Context, attributeSet: AttributeSet?=null,defStyleAttr:Int?=null, defStyleRes: Int?){
+
+}
